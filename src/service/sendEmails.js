efb801926b764826c60f482b3d2910d9e29f4814
Mostafahmed21 <decha5070@gmail.com>
2025-03-21 17:27:40 +0200

    send Emails

--- src/service/sendEmails.js
+++ src/service/sendEmails.js
@@ -1,1 +1,22 @@
-// hello world
+import nodemailer from 'nodemailer';
+import { asyncHandler } from '../utils/globalErrorHandling/index.js';
+
+export const sendEmails = asyncHandler(async( to , subject , html )=>{
+    const transporter  = nodemailer.createTransport({
+        service: 'gmail' ,
+        auth:{
+            user : process.env.EMAIL,
+            pass : process.env.PASSWORD
+        }
+    })
+    const info  = await transporter.sendMail({
+        from : process.env.EMAIL ,
+        to : to ,
+        subject ,
+        html  
+    })
+    if(info.accepted.length){
+        return true;
+    }
+    return false;
+})
