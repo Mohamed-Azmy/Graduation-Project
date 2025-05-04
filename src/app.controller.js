f2a2ac501bdc9407a690e2c901a0439f09df8738
mohamed <mazmy1279@gmail.com>
2025-05-04 17:53:56 +0300

    handle helmet

--- src/app.controller.js
+++ src/app.controller.js
@@ -4,13 +4,22 @@ import doctorDashBoard from "./modules/doctorDashBoard/doctor.controller.js"
 import userDashBoard from "./modules/userDashBoard/user.controller.js"
 import { globalErrorHandling } from "./utils/globalErrorHandling/index.js"
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const bootStrap=async(app,express)=>{
+    
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, 
+        max: 100, 
+        message: "Too many requests from this IP, please try again later.",
+      });
 
     app.use(express.json({ limit: "500mb" }));
     app.use(express.urlencoded({ limit: "500mb", extended: true }));
-
+    app.use(helmet());
     app.use(cors());
+    app.use(limiter())
 
     app.use("/doctor",doctorDashBoard)
     app.use("/users",userDashBoard)
