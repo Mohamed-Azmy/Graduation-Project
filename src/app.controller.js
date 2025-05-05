c9c56058f512119dee561c693091aeafd810c965
Zeyad-Ashraf <132409510+Zeyad-Ashraf@users.noreply.github.com>
2025-05-05 09:45:04 +0300

    Update app.controller.js

--- src/app.controller.js
+++ src/app.controller.js
@@ -18,11 +18,7 @@ const bootStrap=async(app,express)=>{
     app.use(express.json({ limit: "500mb" }));
     app.use(express.urlencoded({ limit: "500mb", extended: true }));
     app.use(helmet());
-    app.use(cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }));
+    app.use(cors('*'));
     app.use(limiter())
 
     app.use("/doctor",doctorDashBoard)
