9972c1d073071ff4482d85aa0df9dc7159db956f
ziad <ziaddash33@gmail.com>
2025-05-05 07:17:00 +0300

    fix cors issue

--- src/app.controller.js
+++ src/app.controller.js
@@ -18,7 +18,7 @@ const bootStrap=async(app,express)=>{
     app.use(express.json({ limit: "500mb" }));
     app.use(express.urlencoded({ limit: "500mb", extended: true }));
     app.use(helmet());
-    app.use(cors());
+    app.use(cors('*'));
     app.use(limiter())
 
     app.use("/doctor",doctorDashBoard)
